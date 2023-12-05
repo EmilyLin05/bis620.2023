73bfb725497d08f540dd9cc6eb3a95a0bcd11136
Emily_Lin <shuqi.lin@yale.edu>
2023-12-04 20:19:34 -0500

    2nd update

---
+++ R/conditions.R
@@ -0,0 +1,11 @@
+#' Conditions Dataset
+#'
+#' A dataset containing information about the conditions studied.
+#'
+#' @format A data frame with 809607 rows and 4 variables, including:
+#' \describe{
+#'   \item{nct_id}{xxx}
+#'   \item{name}{xxx}
+#'   \item{downcase_name}{}
+#' }
+"conditions"
---
+++ R/sponsors.R
@@ -0,0 +1,12 @@
+#' Sponsors Dataset
+#'
+#' A dataset containing information about the sponsors of the studies.
+#'
+#' @format A data frame with 750282 rows and 5 variables, including:
+#' \describe{
+#'   \item{nct_id}{xxx}
+#'   \item{agency_class}{xxx}
+#'   \item{lead_or_collaborator}{xxx}
+#'   \item{name}{xxx}
+#' }
+"sponsors"
